b993ab79527ea95f09d0a1d681598897e900a8e4
Kishan Patel <mail@kishanpatel.dev>
2024-04-17 12:03:27 -0500

    Clean contact and base template

---
+++ content/pages/contact.md
@@ -0,0 +1,6 @@
+Title: Contact
+
+# Let's Talk
+Whether your seeking more information or want to schedule a free consultation, submit this form and we’ll respond within 24 hours.
+
+Email us at [info@kpdata.dev](mailto:info@kpdata.dev)
--- pelicanconf.py
+++ pelicanconf.py
@@ -15,20 +15,6 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (
-    ("Pelican", "https://getpelican.com/"),
-    ("Python.org", "https://www.python.org/"),
-    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
-    ("You can modify those links in your config file", "#"),
-)
-
-# Social widget
-SOCIAL = (
-    ("LinkedIn", "https://www.linkedin.com/company/kp-data-dev/"),
-    ("Email", "mailto:info@kpdata.dev"),
-)
-
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
