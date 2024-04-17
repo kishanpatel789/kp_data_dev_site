63860228fa72b54e0c11727ef5820989d112c8c4
Kishan Patel <mail@kishanpatel.dev>
2024-04-17 09:53:29 -0500

    Remove generated output from VC

--- pelicanconf.py
+++ pelicanconf.py
@@ -38,4 +38,4 @@ DEFAULT_PAGINATION = 10
 ARTICLE_URL = 'blog/{slug}/'
 ARTICLE_SAVE_AS = 'blog/{slug}/index.html'
 PAGE_URL = '{slug}/'
-PAGE_SAVE_AS = 'pages/{slug}/index.html'
+PAGE_SAVE_AS = '{slug}/index.html'
