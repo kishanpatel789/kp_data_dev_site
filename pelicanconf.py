8f9a501d4e827e69fbfc0b48baab5448d553cbd6
Kishan Patel <mail@kishanpatel.dev>
2024-04-24 11:02:10 -0500

    Do not generate category of author pages

--- pelicanconf.py
+++ pelicanconf.py
@@ -25,6 +25,8 @@ ARTICLE_URL = 'blog/{slug}/'
 ARTICLE_SAVE_AS = 'blog/{slug}/index.html'
 PAGE_URL = '{slug}/'
 PAGE_SAVE_AS = '{slug}/index.html'
+AUTHOR_SAVE_AS = ''  # don't generate author pages
+CATEGORY_SAVE_AS = ''  # don't generate category pages
 
 THEME = './theme'
 
