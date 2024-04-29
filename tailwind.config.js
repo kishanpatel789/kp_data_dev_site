7bfb06f0cc541c305ad893aac0e6803a7675961d
Kishan Patel <mail@kishanpatel.dev>
2024-04-29 10:20:55 -0500

    Switch to production tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./theme/templates/**/*.html"],
+  content: ["./theme/templates/**/*.html", "./content/pages/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        kpddmain: "#293c47",
+        kpddsec: "#3f5765",
+        kpddaccent: "#c2ae45"
+      }
+    },
   },
   plugins: [],
 }
