a32f421765151c66e87167185621f270316e6806
Kishan Patel <mail@kishanpatel.dev>
2024-05-21 14:07:39 -0500

    Do not generate tag pages

--- pelicanconf.py
+++ pelicanconf.py
@@ -27,6 +27,7 @@ PAGE_URL = '{slug}/'
 PAGE_SAVE_AS = '{slug}/index.html'
 AUTHOR_SAVE_AS = ''  # don't generate author pages
 CATEGORY_SAVE_AS = ''  # don't generate category pages
+TAG_SAVE_AS = ''  # don't generate tag pages
 DRAFT_URL = 'drafts/{slug}/'
 DRAFT_SAVE_AS = 'drafts/{slug}/index.html'
 
